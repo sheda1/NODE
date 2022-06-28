3ddf258cb17e6274cb3d111a035b7c0011f2a8d3
Shahida <shahidamulla0403@gmail.com>
2022-06-28 17:16:26 +0530

    assign1Ans7

--- createServer.js
+++ createServer.js
@@ -19,9 +19,54 @@ const server = http.createServer((request,response) => {
 server.listen(4000);
 */
 //var http = require('http');
+/*
 
 http.createServer(function (req, res) {
     
     res.write('shahida');
     res.end();
 }).listen(8080);
+
+*/
+/*
+const server = http.createServer((request,response) => {
+    console.log(request.url,request.method,request.headers);
+    response.setHeader('Content-Type','text/html');
+    response.write('<Html>');
+    response.write('<Head><title>My first Page</title><html>');
+    response.write('<body><h1>"Welcome to my Node Js project"</h1></body>');
+    response.write('</Html>');
+    response.end();
+});
+server.listen(3000);
+*/
+
+const server = http.createServer((request,response) => {
+    const req = request.url;
+    //console.log(req);
+    if(req == '/home'){
+        //response.write("Welcome home");
+        response.write('<Html>');
+        response.write('<Head><title>My first Page</title><html>');
+        response.write('<body><h1>"Welcome home"</h1></body>');
+        response.write('</Html>');
+        response.end();
+    }
+     if(req == '/about'){
+       // response.write("Welcome to About Us page");
+        response.write('<Html>');
+        response.write('<Head><title>My first Page</title><html>');
+        response.write('<body><h1>"Welcome to About Us page"</h1></body>');
+        response.write('</Html>');
+        response.end();
+    }
+     if(req == '/node'){
+        //response.write("Welcome to my Node Js project");
+        response.write('<Html>');
+        response.write('<Head><title>My first Page</title><html>');
+        response.write('<body><h1>"Welcome to my Node Js project"</h1></body>');
+        response.write('</Html>');
+        response.end();
+    } 
+});
+server.listen(3000);
