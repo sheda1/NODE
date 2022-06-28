db32fafa6c96aae6373239f199b2efb87a41e27d
Shahida <shahidamulla0403@gmail.com>
2022-06-28 16:39:22 +0530

    assign1Ans6

--- createServer.js
+++ createServer.js
@@ -11,9 +11,17 @@ http.createServer(function(request,response){
 
 });
 */
-
+/*
 const server = http.createServer((request,response) => {
     console.log('Shahida');
     
 });
 server.listen(4000);
+*/
+//var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.write('shahida');
+    res.end();
+}).listen(8080);
